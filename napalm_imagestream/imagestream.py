592c13a1374dd875a4d09833b4b4dcaa2f2aa9e0
Joshua Snyder <josh@cheetahfox.com>
2021-04-22 22:42:01 +0000

    Get_interfaces and supporting functions

--- napalm_imagestream/imagestream.py
+++ napalm_imagestream/imagestream.py
@@ -16,7 +16,7 @@
 # ImageStream specific changes added by Joshua Snyder.
 
 """
-Napalm driver for ImageStream Oputia.
+Napalm driver for ImageStream Oputia / OpenWrt.
 
 Read https://napalm.readthedocs.io for more information.
 """
@@ -49,6 +49,33 @@ class ImageStreamDriver(NetworkDriver):
         if optional_args is None:
             optional_args = {}
 
+        netmiko_argument_map = {
+            'port': None,
+            'verbose': False,
+            'global_delay_factor': 1,
+            'use_keys': False,
+            'key_file': None,
+            'ssh_strict': False,
+            'system_host_keys': False,
+            'alt_host_keys': False,
+            'alt_key_file': '',
+            'ssh_config_file': None,
+            'secret': password,
+            'allow_agent': False,
+            'fast_cli': True
+        }
+
+        # Build dict of any optional Netmiko args
+        self.netmiko_optional_args = {
+                k: optional_args.get(k, v)
+                for k, v in netmiko_argument_map.items()
+            }
+        self.port = optional_args.get('port', 22)
+        self.sudo_pwd = optional_args.get('sudo_pwd', self.password)
+        self.fast_cli = optional_args.get('fast_cli', True )
+
+
+
     def open(self):
         try:
             self.device = ConnectHandler(device_type='linux',
@@ -73,6 +100,33 @@ class ImageStreamDriver(NetworkDriver):
             'is_alive': self.device.remote_conn.transport.is_active()
         } 
 
+    # Opuntia / Openwrt may not have values for some items here we fill in keys that aren't already present
+    # I kinda hate this... But I don't see any other choice. 
+    def _interfaceFillValues(interface):
+        # if we don't have an interface status set it down
+        if "is_up" not in interface:
+            interface['is_up'] = False
+        # if we don't have an interface enabled set it down    
+        if "is_enabled" not in interface:
+            interface['is_enabled'] = False
+        # Opuntia / Openwrt does not have an interface description field so this will always fill in. 
+        if "description" not in interface:
+            interface['description'] = ''
+        # If we don't have an last_flapped set it to -1 
+        if "last_flapped" not in interface:
+            interface['last_flapped'] = -1.0
+        # Virtual interfaces and bridge interfaces don't really have a speed. So set it to null if we get here. 
+        if "speed" not in interface:
+            interface['speed'] = ''
+        # If mtu is not present fill it in. Should only happen if the interface is admin down.
+        if "mtu" not in interface:
+            interface['mtu'] = ''
+        # Non-Ethernet devices may not have a mac address, VPN interfaces for example. 
+        if "mac_address" not in interface:
+            interface['mac_address'] = ''
+
+        return interface                      
+
     def get_facts(self):     
         facts = dict()
 
@@ -80,7 +134,7 @@ class ImageStreamDriver(NetworkDriver):
 
         facts['uptime'] = uptime
 
-        hardwareinfo = self.device.send_command(' ubus call imagestream hardwareinfo')
+        hardwareinfo = self.device.send_command('ubus call imagestream hardwareinfo')
         """ 
         Check to see if the hardwareinfo ubus call failed. 
         This can fail if we have an old system, are running in a vm 
@@ -108,8 +162,8 @@ class ImageStreamDriver(NetworkDriver):
 
         """ 
         Get the Opuntia / Openwrt Interface list  
-        Note. This is the list of interfaces configured in the uci system and not the Linux interfaces that 
-        might be present at the kernel level. 
+        Note. This is the list of interfaces configured in the uci system and not 
+        the Linux interfaces that might be present at the Linux kernel level. 
         """
         output = self.device.send_command('ubus list network.interface.*')
 
@@ -122,4 +176,50 @@ class ImageStreamDriver(NetworkDriver):
 
         return facts
 
+    def get_interfaces(self):
+        interfaces = dict()
 
+        output = self.device.send_command('ubus list network.interface.*')
+
+        for line in output.splitlines():
+            interface = {}
+            status = self.device.send_command('ubus call ' + line + ' status')
+            status_json = json.loads(status)
+
+            # Gives us the interface name 
+            interface_name = line.split('.')[2]
+
+            # Check a few basic items and set them if present
+            if "up" in status_json:
+                interface['is_up'] = status_json['up']
+            if "autostart" in status_json:
+                interface['is_enabled'] = status_json['autostart'] 
+            if "uptime" in status_json:
+                interface['last_flapped'] = status_json['uptime']      
+
+            #Check the interface is really there and it has a single linux device, ie. it's not a bridge 
+            if status_json['available'] is True:
+                if "l3_device" in status_json or "device" in status_json: 
+                    if "l3_device" in status_json:
+                        kernel_dev = status_json['l3_device']
+                    else:
+                        kernel_dev = status_json['device']    
+
+                    # since we have the kernel device we can get detailed stats from netifd
+                    dev_status = self.device.send_command('ubus call network.device status \'{ "name": "' + kernel_dev + '" }\'')
+                    dev_status_json = json.loads(dev_status)
+
+                    if "mtu" in dev_status_json:
+                        interface['mtu'] = dev_status_json['mtu']
+                    
+                    if "macaddr" in dev_status_json:    
+                        interface['mac_address'] = dev_status_json['macaddr'].upper()
+                    
+                    if "speed" in dev_status_json:
+                        s = re.findall('^[ 0-9]+', dev_status_json['speed'])
+                        if s:
+                            interface['speed'] = s[0]
+                
+            interfaces[interface_name] = _interfaceFillValues(interface)
+
+        return interfaces
